7c986599eee77d5cb65e690a3569dfe8314ec063
Abtahi360 <abtahibinislam2018@gmail.com>
2026-01-27 15:39:19 +0600

    successfully return data from mysql with JPA

--- dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
+++ dev-spring-boot/03-spring-boot-hibernate-jpa-crud/01-cruddemo-student/src/main/java/com/luv2code/cruddemo/CruddemoApplication.java
@@ -6,23 +6,39 @@ import org.hibernate.boot.archive.scan.spi.ScanOptions;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CruddemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CruddemoApplication.class, args);
-	}
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
 
     @Bean
     public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
         return runner ->{
             //createMultipleStudents(studentDAO);
 
-            readStudent(studentDAO);
+            //readStudent(studentDAO);
+
+            queryForStudents(studentDAO);
         };
     }
+
+    private void queryForStudents(StudentDAO studentDAO) {
+        // get a list of students
+        List<Student> theStudents = studentDAO.findAll();
+
+        // display list of students
+        for (Student tempStudent : theStudents) {
+            System.out.println(tempStudent);
+        }
+    }
+
     private void readStudent(StudentDAO studentDAO) {
         // create a student object
         System.out.println("Creating new student object...");
