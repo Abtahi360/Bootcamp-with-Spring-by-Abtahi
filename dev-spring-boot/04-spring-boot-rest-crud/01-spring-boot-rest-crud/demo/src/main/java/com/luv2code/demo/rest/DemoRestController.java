d1c563443995ddebd7e84534b427a7382de5a3cb
Abtahi Islam <143752829+Abtahi360@users.noreply.github.com>
2026-04-14 02:02:34 +0600

    Add initial Spring Boot application structure with Maven support

--- dev-spring-boot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/luv2code/demo/rest/DemoRestController.java
+++ dev-spring-boot/04-spring-boot-rest-crud/01-spring-boot-rest-crud/demo/src/main/java/com/luv2code/demo/rest/DemoRestController.java
@@ -1,4 +1,15 @@
 package com.luv2code.demo.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
 public class DemoRestController {
+
+    // add code for the "/sayHello" endpoint
+    @RequestMapping("/hello")
+    public String sayHello(){
+        return "Hello World..!";
+    }
 }
